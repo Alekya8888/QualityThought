bbf39db283686f3c9eab724fa9ad1b832847e6e1
Alekya8888 <159680862+Alekya8888@users.noreply.github.com>
2025-09-25 15:01:35 +0530

    Add files via upload

---
+++ PrimeCheck.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class PrimeCheck 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number :");
+		int n = sc.nextInt();//5
+		
+		boolean flag=true;
+		for(int i=2;i<=n/2;i++)
+		{
+			if(n%i==0)
+			{
+				flag=false;
+				break;
+			}
+		}
+		if(flag==true)
+			System.out.println("It is a prime number");
+		else
+			System.out.println("It is not a prime number");
+	}
+}
