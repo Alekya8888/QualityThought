be92fab2b9081cf47c09eeddec64efd13c165002
Alekya8888 <159680862+Alekya8888@users.noreply.github.com>
2025-09-25 14:37:49 +0530

    Update CountVowelConsonant.java

--- CountVowelConsonant.java
+++ CountVowelConsonant.java
@@ -8,17 +8,6 @@ class CountVowelConsonant
 		String str = sc.nextLine().toLowerCase();
 		
 		int vowels = 0, consonants = 0;
-		/* if(str == 'a' || str == 'e' || str == 'i' || str == 'o' || str == 'u' || str == 'A' || str == 'E' || str == 'I' || str == 'O' || str == 'U')
-		{
-			count++;
-			System.out.println("Vowel count: " +count);
-		}
-		if(str == 'b' || str == 'c' || str == 'd' || str == 'f' || str == 'g' || str == 'h' || str == 'j' || str == 'k' || str == 'l' || str == 'm' ||	str == 'n' || str == 'p' || str == 'q' || str == 'r' || str == 's' || str == 't' || str == 'v' || str == 'w' || str == 'x' || str == 'y' || str == 'z' || str == 'B' || str == 'C' || str == 'D' || str == 'F' || str == 'G' || str == 'H' || str == 'J' || str == 'K' || str == 'L' || str == 'M' ||	str == 'N' || str == 'P' || str == 'Q' || str == 'R' || str == 'S' || str == 'T' || str == 'V' || str == 'W' || str == 'X' || str == 'Y' || str == 'Z' )
-		{
-			count++;
-			System.out.println("Consonant count is: " +count);
-		}
-		*/
 		for(char ch : str.toCharArray())
 		{
 			if (Character.isLetter(ch)) 
@@ -48,3 +37,4 @@ class CountVowelConsonant
 		
 	}
 }
+
